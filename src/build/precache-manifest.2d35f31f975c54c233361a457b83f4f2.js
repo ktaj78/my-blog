1b97fc022f8da61e12bebecd8c52aa2de8b82fec
Karim Taj <karimtaj@Karims-iMac.local>
2020-04-06 21:34:59 -0400

    first commit

---
+++ src/build/precache-manifest.2d35f31f975c54c233361a457b83f4f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67614e1ee47d306d867e35f50ca6edb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58749dd9b35813abfb15",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "e2d7fed56036f2104b0c",
+    "url": "/static/js/2.ac292a11.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ac292a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58749dd9b35813abfb15",
+    "url": "/static/js/main.003e6c25.chunk.js"
+  },
+  {
+    "revision": "209d7516a220ea251298",
+    "url": "/static/js/runtime-main.cc5c5978.js"
+  }
+]);
